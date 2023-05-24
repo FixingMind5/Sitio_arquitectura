429821fcabb8f245c20575bb3bebf0d7c36b2261
FixingMind5 <aguilarmanu5@gmail.com>
2023-05-24 10:17:34 -0600

    Starting buildings

--- Sitio_arquitectura/Projects/admin.py
+++ Sitio_arquitectura/Projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Project, Building, UrbanSpace
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project)
+admin.site.register(Building)
+admin.site.register(UrbanSpace)
--- Sitio_arquitectura/Projects/views.py
+++ Sitio_arquitectura/Projects/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def buildings(request):
+    return render(request, 'building.html')
